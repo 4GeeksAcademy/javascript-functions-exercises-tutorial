555e234fd703a89272b470917056155240c79d9c
Luis Rivera <luis.rivera846@gmail.com>
2019-09-19 19:30:00 +0000

    added change for 10

--- exercises/10-Array-Methods/app.js
+++ exercises/10-Array-Methods/app.js
@@ -1,6 +1,6 @@
 var names = ['John', 'Kenny', 'Tom', "Bob", 'Dilan'];
 
-function sortNames(arr){
+function sortNames(){
 
 }
 
--- exercises/10-Array-Methods/tests.js
+++ exercises/10-Array-Methods/tests.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 it('Did you return using the parameter with the correct SORTING method??', () => {
-   const regex = /return\s*arr.sort\(\)/gm;
+   const regex = /return\s*\w+.sort\(\)/gm;
    const fileContent = fs.readFileSync('./exercises/10-Array-Methods/app.js');
     const match = regex.exec(fileContent);
 
