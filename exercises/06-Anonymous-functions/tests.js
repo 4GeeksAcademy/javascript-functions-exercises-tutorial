aa4764958dbfeee7d2b497e4f6079259333da246
Omonroy36 <monroyorlando.36@gmail.com>
2020-05-11 22:26:04 +0000

    Test on exercise 6 fixed

--- exercises/06-Anonymous-functions/tests.js
+++ exercises/06-Anonymous-functions/tests.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const rewire = require('rewire');
-
-it('Did you call the anonymous function multy with the correct parameters inside a console.log()?', function () {
-
-    const regex = /console.log\(\s*multy\s*\(\s*(\d+)\s*,\s*(\d+)\s*\s*\)\)/gm;
+global.console.log = console.log = jest.fn(text => null);
+it('Call the console.log function once', function () {
+    const app = require('./app.js');
+     expect(console.log).toHaveBeenCalledTimes(1);
+});
+it('The console.log output does not match what we expected', function () {
+    const app = require('./app.js');
+    expect(console.log).toHaveBeenCalledWith(324234 * 47);
+});
+it('Did you called the function multy with the two parameters?', () => {
+    const regex = /multy\s*\(\s*324234\s*,\s*47\s*\)/gm;
     const fileContent = fs.readFileSync('./exercises/06-Anonymous-functions/app.js');
     const match = regex.exec(fileContent);
-
-    expect(parseInt(match[1],10)).toBe(324234);
-    expect(parseInt(match[2],10)).toBe(47);
+    expect(match).toEqual(expect.anything());
 });
-
