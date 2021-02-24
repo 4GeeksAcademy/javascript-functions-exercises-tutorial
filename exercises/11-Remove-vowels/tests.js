2ced7fa5be2414f60c1d6d7f1d56517d5cc70adf
leisyarcia <leisyarcia@gmail.com>
2021-02-24 15:44:25 +0000

    PR

--- exercises/11-Remove-vowels/tests.js
+++ exercises/11-Remove-vowels/tests.js
@@ -8,7 +8,7 @@ it('Did you declare an arrow function and save it to constant named rapid?', fun
     const app = rewire('./app.js');
     let rapid = app.__get__("rapid");
    const regex = /const\s*rapid\s*=\s*\(?\s*(\w+)\s*\)?\s*=>/gm;
-   const fileContent = fs.readFileSync('./exercises/07-Arrow-Functions/app.js');
+   const fileContent = fs.readFileSync('./exercises/11-Remove-vowels/app.js');
     const match = regex.exec(fileContent);
 
     expect(match).toBeTruthy();
@@ -20,7 +20,7 @@ it('Did you declare an arrow function and save it to constant named rapid?', fun
 it('Did you create a for loop to iterate through the string and remove the vowels?', function () {
 
    const regex = /for\s*/gm;
-   const fileContent = fs.readFileSync('./exercises/07-Arrow-Functions/app.js');
+   const fileContent = fs.readFileSync('./exercises/11-Remove-vowels/app.js');
     const match = regex.exec(fileContent);
 
     expect(match).toBeTruthy();
