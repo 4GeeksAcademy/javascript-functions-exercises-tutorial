7ecdeaf0117f3d58c93d73edf2838f2db2ba97d3
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-01-19 16:15:29 -0400

    Update tests.js

--- exercises/10-Array-Methods/tests.js
+++ exercises/10-Array-Methods/tests.js
@@ -7,18 +7,18 @@ test('The function should use the sort method to sort the arrays', () => {
     expect(regex.exec(fileContent)).toBeTruthy();
 })
 
-test('The function should sort the array and then return the array sorted. testing with a different array', () => {
-    const file = rewire("./app.js");
-    const sortNames = file.__get__('sortNames');
-    let names = ['John', 'Kenny', 'Tom', "Bob", 'Dilan'];
-    let sortedNames = ['Bob', 'Dilan', 'John', 'Kenny', 'Tom'];
-    expect(sortNames(names)).toEqual(sortedNames);
-});
-
 test('The function should sort the array and then return the array sorted.', () => {
     const file = rewire("./app.js");
     const sortNames = file.__get__('sortNames');
     let names = ['Tomas', 'Alejandro', 'Jake', "Mark"];
     let sortedNames = ['Alejandro', 'Jake', 'Mark', 'Tomas'];
+    expect(sortNames(names)).toEqual(sortedNames);
+});
+
+test('The function should sort the array and then return the array sorted. testing with a different array', () => {
+    const file = rewire("./app.js");
+    const sortNames = file.__get__('sortNames');
+    let names = ['John', 'Kenny', 'Tom', "Bob", 'Dilan'];
+    let sortedNames = ['Bob', 'Dilan', 'John', 'Kenny', 'Tom'];
     expect(sortNames(names)).toEqual(sortedNames);
 });
