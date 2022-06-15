9105b11bb6b7c59bfa8d09f98435ef5b7a2c4ee1
Sadiel0 <sadielalmanza@outlook.com>
2022-06-15 22:19:36 +0000

    added test to check for function existance & changed filecontent to correct exercise

--- exercises/05-Anonymous-functions/tests.js
+++ exercises/05-Anonymous-functions/tests.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
 const rewire = require('rewire');
 global.console.log = console.log = jest.fn(text => null);
+
+it('The function multy must exist ', () => {
+    const app = rewire('./app.js');
+    const multy = app.__get__('multy');
+    expect(multy).toBeTruthy();
+})
+
 it('Call the console.log function once', function () {
     const app = require('./app.js');
      expect(console.log).toHaveBeenCalledTimes(1);
 });
+
 it('The console.log output does not match what we expected', function () {
     const app = require('./app.js');
     expect(console.log).toHaveBeenCalledWith(324234 * 47);
 });
+
 it('Did you called the function multy with the two parameters?', () => {
     const regex = /multy\s*\(\s*324234\s*,\s*47\s*\)/gm;
-    const fileContent = fs.readFileSync('./exercises/06-Anonymous-functions/app.js');
+    const fileContent = fs.readFileSync('./exercises/05-Anonymous-functions/app.js');
     const match = regex.exec(fileContent);
     expect(match).toEqual(expect.anything());
 });
