54d7cf9be8c82923772101aa32d20518e840c21d
Sadiel0 <sadielalmanza@outlook.com>
2022-06-21 20:45:13 +0000

    added test to check for multiple values

--- exercises/04-Defining-vs-Calling-a-function/app.js
+++ exercises/04-Defining-vs-Calling-a-function/app.js
@@ -1,6 +1,8 @@
 //Define the function called "multi" that expects 2 parameters:
-
+function multi (a, b) {
+  return a * b
+}
 
   //don't edit anything below this line
-  var returnValue = multi(7,53812212);
+  var returnValue = multi(7,12);
   console.log(returnValue);
--- exercises/05-Anonymous-functions/tests.js
+++ exercises/05-Anonymous-functions/tests.js
@@ -18,9 +18,25 @@ it('The console.log output does not match what we expected', function () {
     expect(console.log).toHaveBeenCalledWith(324234 * 47);
 });
 
+
+it('Make sure the function is doing what is supposed to', function () {
+    const multy = rewire ('./app.js').__get__("multy");
+    let solution = multy(3,6);
+    let solution1 = multy(3,100);
+    let solution2 = multy(7,300);
+
+    expect(solution).toBe(18);
+    expect(solution1).toBe(300);
+    expect(solution2).toBe(2100);
+    
+});
+
 it('Did you called the function multy with the two parameters?', () => {
     const regex = /multy\s*\(\s*324234\s*,\s*47\s*\)/gm;
     const fileContent = fs.readFileSync('./exercises/05-Anonymous-functions/app.js');
     const match = regex.exec(fileContent);
     expect(match).toEqual(expect.anything());
 });
+
+
+
