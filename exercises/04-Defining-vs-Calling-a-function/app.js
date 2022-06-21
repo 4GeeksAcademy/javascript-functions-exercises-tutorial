400a14d440cbf3e370cc3b442781df9c79e0ac5c
Sadiel0 <sadielalmanza@outlook.com>
2022-06-21 20:47:55 +0000

    removed solution

--- exercises/04-Defining-vs-Calling-a-function/app.js
+++ exercises/04-Defining-vs-Calling-a-function/app.js
@@ -1,7 +1,4 @@
 //Define the function called "multi" that expects 2 parameters:
-function multi (a, b) {
-  return a * b
-}
 
   //don't edit anything below this line
   var returnValue = multi(7,12);
