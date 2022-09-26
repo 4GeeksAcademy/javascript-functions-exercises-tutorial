3fe4edb86f443b0a231f2b35d11e8126db55e91c
Tomas Gonzalez <56565994+tommygonzaleza@users.noreply.github.com>
2022-09-26 17:46:51 -0400

    Update solution.hide.js

--- exercises/07-Function-that-returns/solution.hide.js
+++ exercises/07-Function-that-returns/solution.hide.js
@@ -1,7 +1,7 @@
-var dollarToEuro = function(dollarValue){
+let dollarToEuro = function(dollarValue){
 	return dollarValue * 0.89;
 }
-var euroToYen = function(euroValue){
+let euroToYen = function(euroValue){
 	return euroValue * 124.15;
 }
 //***** YOUR CODE BELOW ↓ ******///
