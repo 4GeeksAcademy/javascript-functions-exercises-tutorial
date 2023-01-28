85264bc301c324cd9e53b519f98efd8fc036d558
Oscar Miras <oscar.miras@gmail.com>
2023-01-28 18:37:13 +0100

    Solution in the exercise
    I think that the solution was copied to the exercise by mistake

--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,11 +1,4 @@
-const rapid = (myString) => {
-    let consonants = [];
-    for(let letter of myString){
-        if(['a','e','i','o','u'].includes(letter) == false)
-            consonants.push(letter)
-    }
-    return consonants.join('');
-};
+// Code below your 'rapid' function
 
 // Work above this line; do not change code below
 let str = "John";
