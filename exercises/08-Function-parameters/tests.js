12861a3c49b18a4f5c444a219ca163b600b5fb67
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-07-05 11:18:31 -0400

    Update tests.js
    fixed grammatical errors in print statement

--- exercises/08-Function-parameters/tests.js
+++ exercises/08-Function-parameters/tests.js
@@ -14,17 +14,17 @@ it("The function 'renderPerson' should return something" , function () {
 });
 
 it("The function 'renderPerson' should return the expected output. Tesing with Bob" , function () {
-    expect(renderPerson('Bob', '05/22/1983', 'green', 23, 'male')).toBe("Bob is a 23 years old male born in 05/22/1983 with green eyes")
+    expect(renderPerson('Bob', '05/22/1983', 'green', 23, 'male')).toBe("Bob is a 23 year old male born on 05/22/1983 with green eyes")
 });
 
 it("The function 'renderPerson' should return the expected output. Tesing with Katherine" , function () {
-    expect(renderPerson('Katherine', '06/05/2004', 'green', 17, 'female')).toBe("Katherine is a 17 years old female born in 06/05/2004 with green eyes")
+    expect(renderPerson('Katherine', '06/05/2004', 'green', 17, 'female')).toBe("Katherine is a 17 year old female born on 06/05/2004 with green eyes")
 });
 
 it("Print the function 'renderPerson' in console to see the result" , function () {
     require("./app.js");
     expect(console.log.mock.calls.length).toBe(1)
-    expect(console.log).toHaveBeenCalledWith("Bob is a 23 years old male born in 05/22/1983 with green eyes");
+    expect(console.log).toHaveBeenCalledWith("Bob is a 23 year old male born on 05/22/1983 with green eyes");
 });
 
 
