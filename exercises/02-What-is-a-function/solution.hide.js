5b6303309395e962e27ddb9671fbad00be3b802e
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-08-16 16:43:13 +0200

    Update solution.hide.js

--- exercises/02-What-is-a-function/solution.hide.js
+++ exercises/02-What-is-a-function/solution.hide.js
@@ -2,14 +2,6 @@ function sum(number1,number2) {
     return number1 + number2;
 }
 
-let total = sum(2,3);
-let total2 = sum(5,10);
-let total3 = sum(2,3);
-
-
-console.log(total);
-console.log(total2);
-console.log(total3);
-
+// Your code here
 let superduper = sum(3445324,53454423);
 console.log(superduper);
