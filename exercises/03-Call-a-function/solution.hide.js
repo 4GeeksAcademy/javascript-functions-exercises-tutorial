c42759b590789079308f1fbdca9c353adb7b7140
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-08-16 17:03:58 +0200

    Update solution.hide.js

--- exercises/03-Call-a-function/solution.hide.js
+++ exercises/03-Call-a-function/solution.hide.js
@@ -1,13 +1,9 @@
-function calculateArea(length,edge)
-{
-	return length * edge;
+function calculateArea(length, width) {
+	return length * width;
 }
-// Your code Below this line:
 
+// Your code below this line:
 
 let squareArea1 = calculateArea(4,4);
 let squareArea2 = calculateArea(2,2);
 let squareArea3 = calculateArea(5,5);
-
-
-
