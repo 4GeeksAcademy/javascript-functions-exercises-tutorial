744f637600dfcee1304efeaabaaa4cbd93e65907
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-08-16 16:36:43 +0200

    Update solution.hide.js

--- exercises/02-What-is-a-function/solution.hide.js
+++ exercises/02-What-is-a-function/solution.hide.js
@@ -2,9 +2,9 @@ function sum(number1,number2) {
     return number1 + number2;
 }
 
-var total = sum(2,3);
-var total2 = sum(5,10);
-var total3 = sum(2,3);
+let total = sum(2,3);
+let total2 = sum(5,10);
+let total3 = sum(2,3);
 
 
 console.log(total);
