3f809f703d8c4210a86c8f07305307e16cc4290c
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-08-16 16:42:49 +0200

    Update app.js

--- exercises/02-What-is-a-function/app.js
+++ exercises/02-What-is-a-function/app.js
@@ -2,11 +2,4 @@ function sum(number1,number2) {
     return number1 + number2;
 }
 
-let total = sum(2,3);
-let total2 = sum(5,10);
-let total3 = sum(2,3);
-
-
-console.log(total);
-console.log(total2);
-console.log(total3);
+// Your code here
