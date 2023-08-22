4da700336fb60a7cd582857c346608f6f95a8f89
Jose Mora <109150320+josemoracard@users.noreply.github.com>
2023-08-22 18:45:39 +0200

    Update app.js

--- exercises/07-Function-that-returns/app.js
+++ exercises/07-Function-that-returns/app.js
@@ -1,7 +1,7 @@
-let dollarToEuro = function(dollarValue){
+let dollarToEuro = function(dollarValue) {
 	return dollarValue * 0.89;
 }
-let euroToYen = function(euroValue){
+let euroToYen = function(euroValue) {
 	return euroValue * 124.15;
 }
 ///***** YOUR CODE BELOW ↓ ******///
